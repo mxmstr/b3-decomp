c07b8ee4ac52c1a33232d544bb8b41a62539515b
mxmstr <lynch.er18@gmail.com>
2025-10-23 23:36:28 -0400

    collisionkdtree 1

--- src/b3/CGtCollisionKDTree.cpp
+++ src/b3/CGtCollisionKDTree.cpp
@@ -1,9 +1,13 @@
 #include "common.h"
 
+// CGtCollisionKDTree::FixUp()	(GtCollisionKDTree.cpp)
 INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BE8D0);
 
+// CGtCollisionKDTree::IntersectSphere(GtMathPs2::CGtSphere,bool (*)(const CGtPolygonIntersectionResult&, void*),void*) const	(GtCollisionKDTree.cpp)
 INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BE9B0);
 
+// CGtCollisionKDTree::IntersectLine(const CGtLine&,bool (*)(const CGtPolygonIntersectionResult&, void*),void*) const	(GtCollisionKDTree.cpp)
 INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BEF60);
 
+// CGtCollisionKDTree::IntersectLineNearest(const CGtLine&,CGtPolygonIntersectionResult*) const	(GtCollisionKDTree.cpp)
 INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BF720);
