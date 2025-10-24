a29b66c666bb7368ea84bfc949b973b2ca0ac36e
mxmstr <lynch.er18@gmail.com>
2025-10-23 23:32:07 -0400

    collisionkdtree 1

---
+++ src/b3/CGtCollisionKDTree.cpp
@@ -0,0 +1,9 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BE8D0);
+
+INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BE9B0);
+
+INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BEF60);
+
+INCLUDE_ASM("asm/nonmatchings/b3/CGtCollisionKDTree", func_003BF720);
--- src/b3/CGtPolygonSoup.cpp
+++ src/b3/CGtPolygonSoup.cpp
@@ -1,20 +1,14 @@
 #include "common.h"
 #include "b3/CGtPolygonSoup.h"
 
-void CGtPolygonSoup::FixUp()
+void CGtPolygonSoup16::FixUp()
 {
-    // By performing the entire operation for m_pMember1 in one statement,
-    // we encourage the compiler to use and then discard the temporary register.
     this->m_pMember1 = reinterpret_cast<Member1*>((uint32_t)this + (uint32_t)this->m_pMember1);
-
-    // This allows the compiler to reuse the same register for the second operation,
-    // which is key to matching the original assembly. The compiler is then
-    // smart enough to place the final 'store word' into the branch delay slot.
     this->m_pMember2 = reinterpret_cast<Member2*>((uint32_t)this + (uint32_t)this->m_pMember2);
 }
 
-// void CGtPolygonSoup__FixUp(int32_t *a1)
-// {
-//   *a1 += (int32_t)a1;
-//   a1[1] += (int32_t)a1;
-// }
+void CGtPolygonSoup::FixUp()
+{
+    this->m_pMember1 = reinterpret_cast<Member1*>((uint32_t)this + (uint32_t)this->m_pMember1);
+    this->m_pMember2 = reinterpret_cast<Member2*>((uint32_t)this + (uint32_t)this->m_pMember2);
+}
